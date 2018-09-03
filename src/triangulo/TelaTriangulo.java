69f0ee2f5151fd139fb9708d16b250b96efbe968
felipereblin <felipereblin7@gmail.com>
2018-09-03 19:21:11 -0300

    comit on branch 1.1
    Criado classe Triangulo e transferido o código de negócio

--- src/triangulo/TelaTriangulo.java
+++ src/triangulo/TelaTriangulo.java
@@ -15,87 +15,28 @@ public class TelaTriangulo {
 
 
 	public static void main(String args[]) {
+		
+		Triangulo triangulo = new Triangulo();
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Informa o tipo do triangulo");
 
 		System.out.println("Informa o valor do lado a");
-
 		double a = sc.nextDouble();
 
 		System.out.println("Informa o valor do lado b");
-
 		double b = sc.nextDouble();
 
 		System.out.println("Informa o valor do lado c");
-
 		double c = sc.nextDouble();
 
-
-
-		Set<String> propriedades = new HashSet<String>();
-
-		double hipotenusa = a;
-
-		if (hipotenusa < b) {
-
-			hipotenusa = b;
-
-		}
-
-		if (hipotenusa < c) {
-
-			hipotenusa = c;
-
-		}
-
-		if ((a + b + c - hipotenusa) > hipotenusa) {
-
-			propriedades.add("Válido");
-
-			if (a == b || b == c || c == a) {
-
-				if (a == b && b == c) {
-
-					propriedades.add("Equilátero");
-
-				} else {
-
-					propriedades.add("Isósceles");
-
-				}
-
-			}
-
-			if (a != b && b != c && c != a) {
-
-				propriedades.add("Escaleno");
-
-			}
-
-			if (((a * a + b * b + c * c) - hipotenusa * hipotenusa) == hipotenusa
-
-					* hipotenusa) {
-
-				propriedades.add("Retângulo");
-
-			}
-
-		} else {
-
-			propriedades.add("Inválido");
-
-		}
-
-		
+		Set<String> propriedades = triangulo.processaTriangulo(a, b, c);	
 
 		System.out.println("Propriedades do triangulo ->");
 
 		for (String valor : propriedades) {
-
 			System.out.println(valor);
-
 		}
 
 	}
