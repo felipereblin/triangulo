e7e9ebc11fad454cffc2eb99ed3be72fe2d2fd5d
felipereblin <felipereblin7@gmail.com>
2018-09-12 20:42:40 -0300

    commit refactory

--- src/triangulo/Triangulo.java
+++ src/triangulo/Triangulo.java
@@ -11,56 +11,34 @@ public class Triangulo {
 		double hipotenusa = a;
 
 		if (hipotenusa < b) {
-
 			hipotenusa = b;
-
 		}
 
 		if (hipotenusa < c) {
-
 			hipotenusa = c;
-
 		}
 
 		if ((a + b + c - hipotenusa) > hipotenusa) {
-
 			propriedades.add("Válido");
-
 			if (a == b || b == c || c == a) {
-
 				if (a == b && b == c) {
-
 					propriedades.add("Equilátero");
-
 				} else {
-
 					propriedades.add("Isósceles");
-
 				}
-
 			}
 
 			if (a != b && b != c && c != a) {
-
 				propriedades.add("Escaleno");
-
 			}
 
-			if (((a * a + b * b + c * c) - hipotenusa * hipotenusa) == hipotenusa
-
-					* hipotenusa) {
-
+			if (((a * a + b * b + c * c) - hipotenusa * hipotenusa) == hipotenusa * hipotenusa) {
 				propriedades.add("Retângulo");
-
 			}
-
 		} else {
-
 			propriedades.add("Inválido");
-
 		}
 		
 		return propriedades;
 	}
-	
 }
